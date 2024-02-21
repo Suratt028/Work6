1422124573144482ac29738eb0da9d27d96048ee
Suratt028 <154447222+Suratt028@users.noreply.github.com>
2024-02-21 23:55:46 +0700

    Create 3.1.java

---
+++ 3.1.java
@@ -0,0 +1,15 @@
+package C;
+
+public class Truck extends Car {
+    public void m1(){
+        System.out.println("Truck 1");
+    }
+
+    public void m2(){
+        super.m1();
+    }
+
+    public String toString(){
+        return super.toString() + super.toString();
+    }
+}
